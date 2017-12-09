d3b4e7a62bb4e27bc0bc20871a4a0a36cf3ded90
jad <jad.tarabay1@gmail.com>
2017-12-09 22:38:49 +0200

    deploy

--- config/prod.exs
+++ config/prod.exs
@@ -26,11 +26,11 @@ config :server_side_451, ServerSide451Web.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-# config :server_side_451, ServerSide451.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-#   ssl: true
+config :server_side_451, ServerSide451.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
